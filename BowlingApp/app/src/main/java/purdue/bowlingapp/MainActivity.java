ebd85127ba5c9cbb6b94ffc58591b88982e94e2d
PJSmagacz2897 <jsmagac@purdue.edu>
2017-09-24 16:10:02 -0400

    Switching Activities Test

--- BowlingApp/app/src/main/java/purdue/bowlingapp/MainActivity.java
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/MainActivity.java
@@ -1,13 +1,24 @@
 package purdue.bowlingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
