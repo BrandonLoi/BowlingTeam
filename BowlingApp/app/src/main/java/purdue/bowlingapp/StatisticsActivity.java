6d3ab96962e5ad488718f31d899681dc42e88828
aalthof1 <aalthof@purdue.edu>
2017-09-24 21:56:29 -0400

    added skeleton for statistics activity

---
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/StatisticsActivity.java
@@ -0,0 +1,13 @@
+package purdue.bowlingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
