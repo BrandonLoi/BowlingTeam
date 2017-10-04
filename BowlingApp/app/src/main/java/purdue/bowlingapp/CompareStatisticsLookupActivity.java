4db3d4e8f3b22ad0f92c4b70448b380272186f02
anordhof <anordhof@purdue.edu>
2017-10-04 17:18:40 -0400

    Added button functionality to Compare Stats designs

--- BowlingApp/app/src/main/java/purdue/bowlingapp/CompareStatisticsLookupActivity.java
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/CompareStatisticsLookupActivity.java
@@ -3,14 +3,50 @@ package purdue.bowlingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class CompareStatisticsLookupActivity extends AppCompatActivity {
+
+    Button usernameButton;
+    Button emailButton;
+
+    String username;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistics);
-        Intent intent = getIntent();
+        setContentView(R.layout.activity_compare_statistics_lookup);
+
+        usernameButton = (Button) findViewById(R.id.usernameButton);
+        emailButton = (Button) findViewById(R.id.emailButton);
+        username = null;
+        email = null;
+
+        usernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent compareStatisticsActivity = new Intent(CompareStatisticsLookupActivity.this,
+                        CompareStatisticsActivity.class);
+                compareStatisticsActivity.putExtra(MainActivity.welcome_message, username);
+                startActivity(compareStatisticsActivity);
+
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent compareStatisticsActivity = new Intent(CompareStatisticsLookupActivity.this,
+                        CompareStatisticsActivity.class);
+                compareStatisticsActivity.putExtra(MainActivity.welcome_message, username);
+                startActivity(compareStatisticsActivity);
+            }
+        });
     }
 }
