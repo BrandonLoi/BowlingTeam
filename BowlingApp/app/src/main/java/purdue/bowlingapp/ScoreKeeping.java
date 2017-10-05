a5192f9bf900f160d5e2a327ddf3dd8c948337ca
aalthof1 <aalthof@purdue.edu>
2017-10-05 15:23:02 -0400

    Added local variables to the onCreate() method in the score_keeping class for all text views and buttons in the activity

--- BowlingApp/app/src/main/java/purdue/bowlingapp/ScoreKeeping.java
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/ScoreKeeping.java
@@ -2,12 +2,63 @@ package purdue.bowlingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ScoreKeeping extends AppCompatActivity {
+    private double frameCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_keeping);
+
+        //Access all TextViews in the scorekeeping activity
+        // NAMING CONVENTIONS: fXbY = frame X, ball Y
+        TextView f1b1 = (TextView) findViewById(R.id.f1FirstBall);
+        TextView f1b2 = (TextView) findViewById(R.id.f1SecondBall);
+        TextView f2b1 = (TextView) findViewById(R.id.f2FirstBall);
+        TextView f2b2 = (TextView) findViewById(R.id.f2SecondBall);
+        TextView f3b1 = (TextView) findViewById(R.id.f3FirstBall);
+        TextView f3b2 = (TextView) findViewById(R.id.f3SecondBall);
+        TextView f4b1 = (TextView) findViewById(R.id.f4FirstBall);
+        TextView f4b2 = (TextView) findViewById(R.id.f4SecondBall);
+        TextView f5b1 = (TextView) findViewById(R.id.f5FirstBall);
+        TextView f5b2 = (TextView) findViewById(R.id.f5SecondBall);
+        TextView f6b1 = (TextView) findViewById(R.id.f6FirstBall);
+        TextView f6b2 = (TextView) findViewById(R.id.f6SecondBall);
+        TextView f7b1 = (TextView) findViewById(R.id.f7FirstBall);
+        TextView f7b2 = (TextView) findViewById(R.id.f7SecondBall);
+        TextView f8b1 = (TextView) findViewById(R.id.f8FirstBall);
+        TextView f8b2 = (TextView) findViewById(R.id.f8SecondBall);
+        TextView f9b1 = (TextView) findViewById(R.id.f9FirstBall);
+        TextView f9b2 = (TextView) findViewById(R.id.f9SecondBall);
+        TextView f10b1 = (TextView) findViewById(R.id.f10FirstBall);
+        TextView f10b2 = (TextView) findViewById(R.id.f10SecondBall);
+        TextView f10b3 = (TextView) findViewById(R.id.f10ThirdBall);
+
+        //Access all buttons on the scorekeeping activity
+        Button b1 = (Button) findViewById(R.id.b1);
+        Button b2 = (Button) findViewById(R.id.b2);
+        Button b3 = (Button) findViewById(R.id.b3);
+        Button b4 = (Button) findViewById(R.id.b4);
+        Button b5 = (Button) findViewById(R.id.b5);
+        Button b6 = (Button) findViewById(R.id.b6);
+        Button b7 = (Button) findViewById(R.id.b7);
+        Button b8 = (Button) findViewById(R.id.b8);
+        Button b9 = (Button) findViewById(R.id.b9);
+        Button bX = (Button) findViewById(R.id.bX);
+        Button b0 = (Button) findViewById(R.id.b0);
+        Button bSpare = (Button) findViewById(R.id.bSpare);
+        Button bSplit = (Button) findViewById(R.id.bSplit);
+        Button bMakeable = (Button) findViewById(R.id.bMakeable);
+        Button bLeft = (Button) findViewById(R.id.bLeft);
+        Button bPocket = (Button) findViewById(R.id.bPocket);
+        Button bRight = (Button) findViewById(R.id.bRight);
+
+        TextView[] tvs = {f1b1,f1b2,f2b1,f2b2,f3b1,f3b2,f4b1,f4b2,f5b1,f5b2,f6b1,f6b2,f7b1,f7b2,f8b1,f8b2,f9b1,f9b2,f10b1,f10b2,f10b3};
+
+
+
     }
 }
