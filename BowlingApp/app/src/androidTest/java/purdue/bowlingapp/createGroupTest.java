60a09868a5523e5bda8df3a53d719b3edee8df00
Brandon Loi <brandon.loi@gmail.com>
2017-10-23 16:16:36 -0400

    Create test to ensure group creation functioning correctly

--- BowlingApp/app/src/androidTest/java/purdue/bowlingapp/createGroupTest.java
+++ BowlingApp/app/src/androidTest/java/purdue/bowlingapp/createGroupTest.java
@@ -1,7 +1,5 @@
 package purdue.bowlingapp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,30 +14,28 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
-public class userCreateTest {
+public class createGroupTest {
     @Test
-    public void userCreateTest() {
+    public void createGroupTest() {
         final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         final DatabaseReference myRef = mDatabase.child("users");//.child(editText.toString());
 
         ValueEventListener listen = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                boolean x = dataSnapshot.hasChild("testUser");
-                assertTrue("user not added to users directory correctly", dataSnapshot.hasChild("testUser"));
+                boolean x = dataSnapshot.hasChild("testGroup");
+                assertTrue("group not created", x);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 //Required, but we don't use. Leave blank
             }
-
         };
-
         myRef.child("testUser").child("username").setValue("testUser");
         myRef.child("testUser").child("password").setValue("123456789");
         myRef.child("testUser").child("email").setValue("test@email.com");
-        final DatabaseReference myRef2 = mDatabase.child("data").child("testUser");
+        DatabaseReference myRef2 = mDatabase.child("data").child("testUser");
         myRef2.child("avgScore").setValue("-1");
         myRef2.child("highScore").setValue("-1");
         myRef2.child("sparePercentage").setValue("-1");
@@ -48,23 +44,23 @@ public class userCreateTest {
         myRef2.child("singlePinPercentage").setValue("-1");
         myRef2.child("strikePercentage").setValue("-1");
         myRef2.child("numGames").setValue("-1");
+        DatabaseReference myRef3 = mDatabase.child("groups");
+
+
+
+        myRef3.addListenerForSingleValueEvent(listen);
+
+
+        myRef3.child("testGroup").child("testUser").setValue("1");
         try {
             Thread.sleep(1000);
         } catch (Exception e) {
             assertTrue("connection failure in test",false);
         }
-        myRef.addListenerForSingleValueEvent(listen);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            assertTrue("connection failure in test",false);
-        }
-        myRef.removeEventListener(listen);
+        myRef3.removeEventListener(listen);
+        myRef3.child("testGroup").setValue(null);
         myRef.child("testUser").setValue(null);
         mDatabase.child("data").child("testUser").setValue(null);
+
     }
 }
-/*
-NOTE: This test will fail when debugging due to the listener mistakenly checking data AGAIN after deleting test data
-
- */
---
+++ BowlingApp/app/src/androidTest/java/purdue/bowlingapp/createUserTest.java
@@ -0,0 +1,70 @@
+package purdue.bowlingapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class createUserTest {
+    @Test
+    public void userCreateTest() {
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference myRef = mDatabase.child("users");//.child(editText.toString());
+
+        ValueEventListener listen = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean x = dataSnapshot.hasChild("testUser");
+                assertTrue("user not added to users directory correctly", dataSnapshot.hasChild("testUser"));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Required, but we don't use. Leave blank
+            }
+
+        };
+
+        myRef.child("testUser").child("username").setValue("testUser");
+        myRef.child("testUser").child("password").setValue("123456789");
+        myRef.child("testUser").child("email").setValue("test@email.com");
+        final DatabaseReference myRef2 = mDatabase.child("data").child("testUser");
+        myRef2.child("avgScore").setValue("-1");
+        myRef2.child("highScore").setValue("-1");
+        myRef2.child("sparePercentage").setValue("-1");
+        myRef2.child("strikePercentage").setValue("-1");
+        myRef2.child("filledPercentage").setValue("-1");
+        myRef2.child("singlePinPercentage").setValue("-1");
+        myRef2.child("strikePercentage").setValue("-1");
+        myRef2.child("numGames").setValue("-1");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            assertTrue("connection failure in test",false);
+        }
+        myRef.addListenerForSingleValueEvent(listen);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            assertTrue("connection failure in test",false);
+        }
+        myRef.removeEventListener(listen);
+        myRef.child("testUser").setValue(null);
+        mDatabase.child("data").child("testUser").setValue(null);
+    }
+}
+/*
+NOTE: This test will fail when debugging due to the listener mistakenly checking data AGAIN after deleting test data
+
+ */
