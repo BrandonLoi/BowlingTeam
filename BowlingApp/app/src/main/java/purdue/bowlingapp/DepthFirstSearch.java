dfdcf5ea8f931071cfb725a02ae792aabb213245
aalthof1 <aalthof@purdue.edu>
2017-11-19 14:30:06 -0500

    implemented depth first search

---
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/DepthFirstSearch.java
@@ -0,0 +1,31 @@
+package purdue.bowlingapp;
+
+/**
+ * Created by Aaron on 11/19/2017.
+ */
+
+public class DepthFirstSearch {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int count;
+
+    public DepthFirstSearch(Graph g,int s) {
+        edgeTo = new int[g.V()];
+        marked = new boolean[g.V()];
+        dfs(g,s);
+    }
+
+    private void dfs(Graph g, int s) {
+        marked[s] = true;
+        for(int w : g.getAdj(s)) {
+            if(!marked[w]) {
+                edgeTo[w]=s;
+                dfs(g,w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+}
