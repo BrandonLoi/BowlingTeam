27a61abff679bf41a3940df6ac44af1ee45a071a
aalthof1 <aalthof@purdue.edu>
2017-11-19 14:18:20 -0500

    created graph class to represent

---
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/Graph.java
@@ -0,0 +1,44 @@
+package purdue.bowlingapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aaron on 11/17/2017.
+ */
+
+public class Graph {
+    //private ArrayList<ArrayList<Integer>> adj;
+    private ArrayList<Integer>[] adj;
+    private int E;
+    private final int V;
+
+    Graph(int V) {
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        this.V = V;
+        this.E = 0;
+        for(int i = 0 ; i < V; i++)
+            adj[i]= new ArrayList<>();
+    }
+
+    public int V() {
+        return this.V;
+    }
+
+    public int E() {
+        return this.E;
+    }
+
+    public void addEdge(int v, int w) {
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public ArrayList<Integer> getAdj(int v) {
+        return adj[v];
+    }
+
+    public int degree(int v) {
+        return adj[v].size();
+    }
+}
