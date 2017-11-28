fe3fd230d694def1002c347f60ae5e64fe037b2f
aalthof1 <aalthof@purdue.edu>
2017-11-28 16:40:52 -0500

    changed naming in pinInput

--- BowlingApp/app/src/main/java/purdue/bowlingapp/PinInput.java
+++ BowlingApp/app/src/main/java/purdue/bowlingapp/PinInput.java
@@ -23,7 +23,7 @@ public class PinInput extends AppCompatActivity {
     boolean[] splits = {false,false,false,false,false,false,false,false,false,false};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_input);
 
