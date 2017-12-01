7fa74aec206441b99bc8decab219fdc9ad234550
PJSmagacz2897 <jsmagac@purdue.edu>
2017-12-01 14:37:47 -0500

    Event based notifications should be working

---
+++ BowlingApp/app/src/androidTest/java/purdue/bowlingapp/notificationTests.java
@@ -0,0 +1,32 @@
+package purdue.bowlingapp;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.renderscript.Sampler;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class notificationTests {
+
+    @Test
+    public void notification_test() {
+
+    }
+
+
+}
